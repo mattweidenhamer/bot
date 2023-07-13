b547e8b607144c20afafecdb0e260dafaeb8af01
Matt Weidenhamer <matthew.weidenhamer@svu.edu>
2023-07-13 17:39:11 -0400

    fixed resolve

--- bot/events/joinGuild.js
+++ bot/events/joinGuild.js
@@ -3,7 +3,7 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.GuildCreate,
   execute(guild) {
-    owner = guild.resolve(guild.ownerID);
+    owner = guild.members.resolve(guild.ownerID);
     console.log(
       "Joined new guild " +
         guild.name +
